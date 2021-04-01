75afe2fffa253f0c93bd2a13ed34aadf8a78c140
NolanEliason <78878336+NolanEliason@users.noreply.github.com>
2021-03-31 21:38:28 -0500

    Elevation Colors
    Change the color of the map marker based on what the elevation of the volcano is.

--- map1.py
+++ map1.py
@@ -10,6 +10,14 @@ html = """<h4>Volcano information:</h4>
 Height: %s m
 """
 
+def color_producer(elevation):
+    if elevation < 1500:
+        return 'green'
+    elif 1500 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 
 map = folium.Map(location=[38.58,-99.09],zoom_start=5, tiles = "Stamen Terrain")
 
@@ -21,7 +29,9 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon,elev):
     iframe = folium.IFrame(html=html % str(el),width=200,height=100)
-    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(iframe), icon = folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(iframe), icon = folium.Icon(color=color_producer(el))))
+
+
 map.add_child(fg)
 
 
